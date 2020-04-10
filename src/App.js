17f377b028d6115c9e32e224505367f87a4a9d41
PiyumalK <piyuk.thilaka@gmail.com>
2020-04-10 12:38:53 +0530

    Completed toggle todo completed function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import Todos from './components/Todos';
 
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Todos />
-//     </div>
-//   );
-// }
-
 class App extends Component {
   state = {
     todos: [
@@ -32,10 +24,20 @@ class App extends Component {
     ]
   }
 
+  // Toggle Completed
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
